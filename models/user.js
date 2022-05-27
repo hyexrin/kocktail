1f8af4dfd0ff6116d918d8c0b324dc5daa26b575
U_U_JIN <yyj9434@naver.com>
2022-05-27 18:41:11 +0900

    models/user.js 파일 추가 (연결되지 않은 파일)

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const { name } = require("ejs");
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+    id : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    pw : {
+        type : String,
+        required : true
+    },
+    name : {
+        type : String,
+        required : true
+    },
+    phone : {
+        type : Number,
+        unique : true
+    }
+});
+
+userSchema.methods.getInfo = () => {
+    return `ID : ${this.id} PW : ${this.pw} NAME : ${this.name} PHONE : ${this.phone}`;
+};
+
+userSchema.methods.findLocalUsers = () => {
+    return this.model("User").find({ pw : this.pw }).exec();
+};
+
+module.exports = mongoose.model("User", userSchema);
