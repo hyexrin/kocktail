1d23ebae42a9eb96c45f31c7f6ef9da5c906e6cc
U_U_JIN <yyj9434@naver.com>
2022-06-07 00:08:05 +0900

    일부 주석 처리

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,6 +6,7 @@ router.get("/products", productsController.index, productsController.productsVie
 router.get("/productsInsert", productsController.productsInsert);
 router.post("/inserted", productsController.create, productsController.redirectView);
 
+// products.ejs 에서 '장바구니' 버튼 누르면 DB cart collection에 저장
 router.post("/cartInsert", cartController.create, cartController.redirectView);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,7 @@ router.get("/login", usersController.login);
 router.post("/login", usersController.authenticate);
 router.get("/logout", usersController.logout, usersController.redirectView);
 
+// myPage를 장바구니 기능 겸용으로 사용
 router.get("/users/:id", usersController.myPage, cartController.cart, usersController.myPageView);
 
 router.get("/users/:id/edit", usersController.edit);
